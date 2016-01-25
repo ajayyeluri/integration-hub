fcfe6cee9a6a90f50e542e377b8d5366158fbf92
ajayyeluri <ayeluri@gmail.com>
2016-01-25 08:58:18 -0500

    first commit

---
+++ src/main/java/com/liquidhub/integration/beans/User.java
@@ -0,0 +1,48 @@
+package com.liquidhub.integration.beans;
+
+public class User {
+
+    String firstName, lastName,  eid, email , telNumber ;
+
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelNumber() {
+        return telNumber;
+    }
+
+    public void setTelNumber(String telNumber) {
+        this.telNumber = telNumber;
+    }
+
+    public String getEid() {
+        return eid;
+    }
+
+    public void setEid(String eid) {
+        this.eid = eid;
+    }
+}
